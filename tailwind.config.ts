2dbe610a8d3e74c634d5bd55aa52e2cbaeba4f08
Ivan Blinov <blinovivan2016@gmail.com>
2024-03-17 11:46:46 +0300

    small refactorings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
 	darkMode: 'selector',
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
