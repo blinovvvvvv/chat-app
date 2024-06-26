216ef0bbdb0f034ceb19d49de3a0ee116b14cfdb
Ivan Blinov <blinovivan2016@gmail.com>
2024-06-26 16:43:33 +0300

    update auth process

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,11 +1,18 @@
+import { User } from '@/src/entities/user';
+import { query } from '@/src/shared/api/queryClient/query';
 import { ReactNode } from 'react';
 import AuthProvider from './auth-provider/AuthProvider';
 import ThemeProvider from './theme-provider/ThemeProvider';
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default async function Providers({ children }: { children: ReactNode }) {
+	const userProfile = await query<User>({
+		url: '/user/profile',
+		auth: true,
+	});
+
 	return (
 		<ThemeProvider>
-			<AuthProvider>{children}</AuthProvider>
+			<AuthProvider user={userProfile}>{children}</AuthProvider>
 		</ThemeProvider>
 	);
 }
--- src/app/auth-provider/AuthProvider.tsx
+++ src/app/auth-provider/AuthProvider.tsx
@@ -1,25 +1,26 @@
 'use client';
 
-import { initializeUserStore } from '@/src/entities/user';
+import { User, initializeUserStore } from '@/src/entities/user';
 import { getNewToken } from '@/src/features/auth/model/actions/new-token.action';
-import {
-	COOKIES_REFRESH_TOKEN_KEY,
-	COOKIES_USER_KEY,
-} from '@/src/shared/const/cookies';
+import { COOKIES_REFRESH_TOKEN_KEY } from '@/src/shared/const/cookies';
+import { isObjectEmpty } from '@/src/shared/utils/is-object-empty/isObjectEmpty';
 import Cookies from 'js-cookie';
-import { ReactNode, useEffect } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 
-export default function AuthProvider({ children }: { children: ReactNode }) {
-	const user = Cookies.get(COOKIES_USER_KEY);
+interface AuthProviderProps {
+	user: User;
+}
 
+export default function AuthProvider({
+	children,
+	user,
+}: PropsWithChildren<AuthProviderProps>) {
 	useEffect(() => {
 		const refreshToken = Cookies.get(COOKIES_REFRESH_TOKEN_KEY);
 
-		if (user) {
-			initializeUserStore(JSON.parse(user));
-		}
+		if (user) initializeUserStore(user);
 
-		if (!user && refreshToken) {
+		if (isObjectEmpty(user) && refreshToken) {
 			getNewToken({ refreshToken });
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
