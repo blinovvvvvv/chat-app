b25a373160d49fd58ad1057d4f84e037bb2305fa
Ivan Blinov <blinovivan2016@gmail.com>
2024-06-29 18:23:50 +0300

    update dialog entity schema

--- src/entities/dialog/model/types/dialog.types.ts
+++ src/entities/dialog/model/types/dialog.types.ts
@@ -1,8 +1,8 @@
+import { IMessage } from '@/src/entities/message';
 import { User } from '@/src/entities/user';
 import { IdTimestamps } from '@/src/shared/types/IdTimestamps';
 
 export interface IDialog extends IdTimestamps {
 	members: User[];
-	//FIXME: add message entity
-	messages: [];
+	messages: IMessage[];
 }
